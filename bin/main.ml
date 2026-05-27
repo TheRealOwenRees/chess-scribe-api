34ebb6f876f1d91c97162734b2aee44754a943fa
Owen <owenrees.dev@gmail.com>
2026-05-27 22:08:24 +0200

    boilerplate for formatting and basic routes

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+let () =
+  Dream.run @@ Dream.logger
+  @@ Dream.router
+       [
+         Dream.get "/" (fun _ -> Dream.html "API Docs");
+         Dream.get "/health" (fun _ -> Dream.html "Health Check");
+         Dream.get "/pdf" (fun _ -> Dream.html "Generate PDF");
+       ]
