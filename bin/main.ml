26567c1e0355363c31a551d07c7a07c0d3d9c9a8
Owen <owenrees.dev@gmail.com>
2026-05-28 16:44:46 +0200

    parse incoming body and generate tex markdown

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,11 @@
 open Lwt.Syntax
 open Yojson.Safe.Util
+open Pgn_logic
 
 let is_dev_mode () =
   Array.to_list Sys.argv
   |> List.exists (fun arg -> arg = "DREAM_ENV=development")
 
-(* Custom CORS Middleware *)
 let cors_middleware next_handler request =
   let allowed_origin =
     if is_dev_mode () then "*" else "https://chess-scribe.org"
@@ -14,14 +14,12 @@ let cors_middleware next_handler request =
   if Dream.method_ request = `OPTIONS then (
     let response = Dream.response "" in
     Dream.add_header response "Access-Control-Allow-Origin" allowed_origin;
-    Dream.add_header response "Access-Control-Allow-Methods"
-      "POST, GET, OPTIONS";
+    Dream.add_header response "Access-Control-Allow-Methods" "POST, GET";
     Dream.add_header response "Access-Control-Allow-Headers"
       "Content-Type, Authorization";
     Dream.set_status response `No_Content;
     Lwt.return response)
   else
-    (* 3. Pass normal requests through and append the origin header *)
     let* response = next_handler request in
     Dream.add_header response "Access-Control-Allow-Origin" allowed_origin;
     Lwt.return response
@@ -34,16 +32,17 @@ let handle_convert request =
 
     let pgn = json |> member "pgn" |> to_string in
     let diagram_clock = json |> member "diagramClock" |> to_bool in
-
-    (* For diagrams, we grab the raw sub-tree as a string to pass along later *)
     let diagrams_json = json |> member "diagrams" in
-    let diagrams_str = Yojson.Safe.to_string diagrams_json in
 
-    print_endline "\n--- Received Request ---";
-    Printf.printf "PGN:\n%s\n\n" pgn;
-    Printf.printf "Diagrams JSON String: %s\n" diagrams_str;
-    Printf.printf "Diagram Clock: %b\n" diagram_clock;
-    print_endline "------------------------\n";
+    let diagrams_data =
+      Yojson.Safe.to_string diagrams_json |> Pgn2tex.parse_diagrams_json
+    in
+
+    let game_tex =
+      Pgn2tex.to_tex pgn ~diagram_data:diagrams_data ~clock:diagram_clock
+    in
+
+    print_endline game_tex;
 
     Dream.json {|{"status": "ok", "message": "Payload parsed successfully!"}|}
   with
@@ -67,14 +66,10 @@ let () =
        [
          Dream.get "/" (fun request ->
              Dream.from_filesystem "static" "doc.html" request);
-         (* Health check (Exact match) *)
          Dream.get "/health" (fun _ -> Dream.respond "OK");
-         (* Specific API Endpoints (Must be ABOVE the wildcard) *)
          Dream.post "/api/v1/pdf" handle_convert;
-         (* Exact match for the docs root *)
          Dream.get "/api/v1" (fun request ->
              Dream.from_filesystem "static" "doc.html" request);
-         (* Dedicated route for swagger.json if accessed outside /api/ *)
          Dream.get "/swagger.json" (fun request ->
              Dream.from_filesystem "static" "swagger.json" request);
        ]
